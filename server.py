d56e36e679a8a6aaa5a8ed3931bc8db7e338ebdd
light_yuan <xl814403471@163.com>
2018-03-30 18:40:26 +0800

    修复初次启动时第一次发送命令无效BUG

--- server.py
+++ server.py
@@ -15,12 +15,14 @@ while True:
 	# print c.recv(1024)
 	print 'Got connection from', addr
 	print "What can i help you to translate messages?"
-	print "input you messages here:"
-	msg = raw_input();
 	while True:
 		try:
 			print "input you messages here:"
 			msg = raw_input();
+
+			if msg == "exit":
+				raise KeyboardInterrupt
+
 			c.send(msg)
 		except KeyboardInterrupt:
 			raise KeyboardInterrupt
